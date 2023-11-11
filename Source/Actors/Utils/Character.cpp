70de7e8098e0a61251a322e1bab538f7c12aad00
jknvlvxs <dev.julio.alves@gmail.com>
2023-11-11 14:17:05 -0300

    feature: add class charachter

---
+++ Source/Actors/Utils/Character.cpp
@@ -0,0 +1,5 @@
+//
+// Created by julio on 11/11/23.
+//
+
+#include "Character.h"
---
+++ Source/Actors/Utils/Character.h
@@ -0,0 +1,16 @@
+//
+// Created by julio on 11/11/23.
+//
+
+#ifndef SUPER_CAPI_FIGHTERS_CHARACTER_H
+#define SUPER_CAPI_FIGHTERS_CHARACTER_H
+
+
+
+class Character {
+
+};
+
+
+
+#endif //SUPER_CAPI_FIGHTERS_CHARACTER_H
